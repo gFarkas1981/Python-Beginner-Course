c53007ac286c09e597be49e88246a2de1ef1e137
gFarkas1981 <info@gfarkas.com>
2020-01-30 13:47:47 +0100

    Functions with parameters

--- Project 1/LetterGradeBot.py
+++ Project 1/LetterGradeBot.py
@@ -1,45 +1,6 @@
-# Letter Grade Bot
+# Parameters
 
-pointsPossible = 37
+def addtwo(num1, num2):
+    print(num1 + num2)
 
-studentName = input("What's your name?\n")
-
-try:
-    score = int(input("What is your score?\n"))
-    print("Valid score!\n")
-except Exception:
-    print("You need to provide a valid score!")
-
-
-
-'''
-A - 100 - 90%
-B - 89 - 80%
-C - 79 - 70%
-D - 69 - 60%
-F - 59 - 0%
-'''
-
-try:
-    age = int(input("What's your age?\n"))
-    print("Valid age!\n")
-except Exception:
-    print("You need to provide a valid age!")
-
-print("You are " + age + " years old.")
-
-print("Student name: " + studentName)
-percent = 100 / (pointsPossible / score)
-print(percent)
-
-if (100 >= percent >= 90):
-    print(studentName +" got grade A")
-elif (89 >= percent >= 80):
-    print(studentName +" got grade B")
-elif (79 >= percent >= 70):
-    print(studentName +" got grade C")
-elif (69 >= percent >= 60):
-    print(studentName +" got grade D")
-elif (59 >= percent >= 0):
-    print(studentName +" got grade F")
-
+addtwo(1, 34)
