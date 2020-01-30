e4dbe978596f7d473ec5f41e29dba92da2ba11d9
gFarkas1981 <info@gfarkas.com>
2020-01-30 16:09:36 +0100

    Immutable lists (tuples)

--- Project 1/LetterGradeBot.py
+++ Project 1/LetterGradeBot.py
@@ -1,18 +1,8 @@
-# Advanced Lists
-# Append, Insert, Update, Delete and length
+# Tuples (immutable lists)
 
-list = ["object1", "object2", "object3", "object4"]
+fixlist = ("fixelement1", 'fixelement2', "fixelement3")
 
-list.append("last one")
 
-list.insert(1, "to the second")
+# fixlist.append
 
-# del(list[3])
-list.remove("object3")
-
-list[1] = "object1+"
-
-del(list[-1])
-
-print(list)
-print(len(list))
+print(fixlist)
