a71ca2dc38291f925439462f0d8acc48da16707b
gFarkas1981 <info@gfarkas.com>
2020-01-30 11:00:35 +0100

    If,else, elif

--- Hello World/hello.py
+++ Hello World/hello.py
@@ -1,17 +1,18 @@
-lyric = """Can't stop, addicted to the shindig
-Chop top, he says I'm gonna win big
-Choose not a life of imitation
-Distant cousin to the reservation
-Defunct, the pistol that you pay for
-This punk, the feeling that you stay for
-In time, I want to be your best friend
-Eastside love is living on the West End
-Knock out, but boy you better come to
-Don't die, you know the truth is some do
-Go write your message on the pavement
-Burn so bright, I wonder what the wave meant
-White heat is screaming in the jungle
-Complete the motion if you stumble
-Go ask the dust for any answers
-Come back strong with 50 belly dancers"""
-print(lyric[-38:])
+sunny = False
+
+if sunny:
+    print("Sunny")
+else:
+    print("Not sunny")
+
+
+a = 5
+
+if a > 4:
+    print("bigger than 4")
+elif a > 0:
+    print("bigger than 0 but less than 4")
+
+if 8 >= a >= 3:
+    print("a is between 3 and 8")
+
