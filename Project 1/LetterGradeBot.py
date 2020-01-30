a22e81197ee47cd8810d6ff4d01003931ed3a24c
gFarkas1981 <info@gfarkas.com>
2020-01-30 14:48:41 +0100

    Checking types

--- Project 1/LetterGradeBot.py
+++ Project 1/LetterGradeBot.py
@@ -1,6 +1,26 @@
-# Parameters
+# Advanced parameters
+
+
+def madlib(name, verb, noun):
+    print(name + " was " + verb + " with a " + noun)
+
+madlib(noun="cheese", name="Nick", verb="running")
 
 def addtwo(num1, num2):
-    print(num1 + num2)
+    total = num1 + num2
+    return total
+    print("Hello")
 
-addtwo(1, 34)
+thenum = addtwo(3,6)
+print(thenum)
+
+def printtype(somekindofstuff):
+    if type(somekindofstuff) == str:
+        print("String")
+    elif type(somekindofstuff) == int:
+        print("Int")
+    elif type(somekindofstuff) == float:
+        print("Float")
+
+
+printtype("")
