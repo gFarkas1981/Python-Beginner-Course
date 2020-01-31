0589f800d83613470da7374efc4a9a8a30cddec7
gFarkas1981 <info@gfarkas.com>
2020-01-31 13:18:16 +0100

    Inheritence

--- Project 2/Hangman.py
+++ Project 2/Hangman.py
@@ -1,27 +1,16 @@
-# Classes and variables
-# init
-
-class Dog:
-
-    def __init__(self, name, age, furcolor):
-        self.name = name
-        self.age = age
-        self.furcolor = furcolor
-
-    def barkhello(self):
-        print("Hello World")
-
-
-dog1 = Dog("Fido", 8, "Brown")
-dog1.barkhello()
-
-print(dog1.furcolor)
+# inheritence
 
 class Car:
     def __init__(self,year, make, model):
         self.year = year
         self.make = make
         self.model = model
-        self.age = 2019 - year
 
-car1 = Car(1981, "Audi", "80")
+    def age(self):
+        return 2016 - self.year
+
+class Mustang(Car):
+    def __init__(self, year):
+        self.year = year
+        self.model = "Mustang"
+        self.make = "Ford"
