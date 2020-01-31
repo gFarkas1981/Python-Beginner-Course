7f7ea46be2a64e18b4a5284ab02ecb7e70536ccf
gFarkas1981 <info@gfarkas.com>
2020-01-31 09:31:34 +0100

    while 2.0
    2 raised to what power, is greater than 1_000_000_000

--- Project 1/LetterGradeBot.py
+++ Project 1/LetterGradeBot.py
@@ -1,21 +1,11 @@
 # While loops
 
-age = 38
+number = 0
+counter = 1
 
-while age < 100:
-    print(age)
-    age += 1
+while number < 1_000_000_000:
+    counter += 1
+    number = 2 ** counter
 
-steps = 1
-secretNumber = '8'
 
-guess = input("Guess a number between 1 and 10!\n")
-
-while guess != secretNumber:
-    steps += 1
-    if int(guess) < int(secretNumber):
-        guess = input("Enter a bigger number!\n")
-    else:
-        guess = input("Enter a smaller number!\n")
-
-print("You win in " + str(steps) + " step's'")
+print(counter)
